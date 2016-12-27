354ef833c955c6e48ab9a2d14e4f675ae8b2f7ed
ucmed <apple_can@yeah.net>
2016-12-27 09:41:25 +0800

    windows zip

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # easyzip
 
-``var easyzip=require('easyzip');``
+```var easyzip=require('easyzip');```
 
-zip a dir,can use
+### zip a dir,can use
 
-``dirzip(dirpath,zippath,callback)``
+```dirzip(dirpath,zippath,callback)```
 
-zippath can be null , if null the zip file will create on tem dir
+### zippath can be null , if null the zip file will create on tem dir
 
-``callback(err,zip_path)``
+```callback(err,zip_path)```
 
-zip a dir and async
+### zip a dir and async
 
-``dirzipAsync(dirpath,zippath).then(path).catch(err)``
+```dirzipAsync(dirpath,zippath).then(path).catch(err)```
+
+
+# update
+
+- 2016.12.26 | v1.0.1 | mac系统上使用压缩
+- 2015.12.27 | v1.0.2 | win系统上使用压缩
--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const checkos = require('./lib/checkos.js');
 const this_os = checkos.get();
 const exec = require('child_process').exec, fs = require('fs'), path = require('path');
 const moment = require('moment'), temp_dir = require('os').tmpdir();
-const Promise = require('bluebird')
+const Promise = require('bluebird');
+const prog = path.resolve(__dirname, 'lib', 'prog'), zip_win = prog + path.sep + '7za.exe';
+
+console.log(prog)
 
 function exists(path) {
     return fs.existsSync(path);
@@ -34,32 +37,37 @@ exports.dirzip = function (dir_path, new_path, callback) {
     if (new_path && isDir(new_path))
         new_path = new_path + path.sep + getnow() + '.zip';
     let new_zip_path = new_path || temp_dir + path.sep + getnow() + '.zip'
+    let result_msg = '';
+
     if (!isDir(dir_path))
         throw 'dir is not exists or is not dir!'
     else {
         switch (this_os) {
-            case checkos.win: ; break;
-            case checkos.linux: ; break;
-            case checkos.mac:
-                let last_path = path.dirname(dir_path), dir_path_split = dir_path.split(path.sep);
-                zip_cmd = `cd ${last_path} && zip -q -r ${new_zip_path} ${dir_path_split[dir_path_split.length - 1]}`;
-                let result = '';
+            case checkos.win:
+                zip_cmd = `${zip_win} a ${new_zip_path}  ${dir_path}`;
                 runcmd(zip_cmd, msg => {
-                    result += msg;
+                    result_msg += msg;
                 }, end => {
                     if (end) {
                         callback(null, new_zip_path)
                     } else
-                        callback(result)
+                        callback(result_msg)
+                });
+                break;
+            case checkos.linux: ; break;
+            case checkos.mac:
+                let last_path = path.dirname(dir_path), dir_path_split = dir_path.split(path.sep);
+                zip_cmd = `cd ${last_path} && zip -q -r ${new_zip_path} ${dir_path_split[dir_path_split.length - 1]}`;
+                runcmd(zip_cmd, msg => {
+                    result_msg += msg;
+                }, end => {
+                    if (end) {
+                        callback(null, new_zip_path)
+                    } else
+                        callback(result_msg)
                 })
                 break;
         }
     }
 }
 exports.dirzipAsync = Promise.promisify(this.dirzip)
-
-this.dirzipAsync('', '').then(path => {
-    console.log(path)
-}).catch(err => {
-    console.log(err)
-})
